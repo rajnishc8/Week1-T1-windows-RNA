1a38f814f5fc32a82e6cf02e0e561c3af43361d7
rajnishc8 <rajnishc8@gmail.com>
2017-12-13 15:25:36 -0600

    Added src  code

---
+++ android/app/src/main/java/com/week1_t1_windows_rna/MainActivity.java
@@ -0,0 +1,15 @@
+package com.week1_t1_windows_rna;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Week1_T1_windows_RNA";
+    }
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('Week1_T1_windows_RNA', () => App);
